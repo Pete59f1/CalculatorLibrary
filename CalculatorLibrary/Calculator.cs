f8319dbddc89436e222ca5d741a66045ed791627
Pete59f1 <pete59f1@edu.eal.dk>
2018-10-03 11:12:05 +0200

    Add project files.

---
+++ CalculatorLibrary/Calculator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorLibrary
+{
+    public class Calculator
+    {
+        public static double Add(double v1, double v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static double Subtract(double v1, double v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static double Multiply(double v1, double v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static double Divide(double v1, double v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static double Sum(double[] numbers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static double Minimum(double[] numbers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static double Maximum(double[] numbers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static double Average(double[] numbers)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CalculatorTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalculatorTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalculatorTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e2db74ae-9607-44a0-8c8c-cbbcc4f126b5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
