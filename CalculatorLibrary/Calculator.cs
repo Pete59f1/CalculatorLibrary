d12f5c83e44d01cbcd0eb6db9299d83a079e2282
Nikolaj Bruun-Hansen <nico9908@edu.eal.dk>
2018-10-03 12:04:48 +0200

    calculator classes tilføjet

--- CalculatorLibrary/Calculator.cs
+++ CalculatorLibrary/Calculator.cs
@@ -10,42 +10,71 @@ namespace CalculatorLibrary
     {
         public static double Add(double v1, double v2)
         {
-            throw new NotImplementedException();
+            return v1 + v2;
         }
 
         public static double Subtract(double v1, double v2)
         {
-            throw new NotImplementedException();
+            return v1 - v2;
         }
 
         public static double Multiply(double v1, double v2)
         {
-            throw new NotImplementedException();
+            return v1 * v2;
         }
 
         public static double Divide(double v1, double v2)
         {
-            throw new NotImplementedException();
+            return v1 / v2;
         }
 
         public static double Sum(double[] numbers)
         {
-            throw new NotImplementedException();
+            double sum = numbers[0];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum = numbers[i] + sum;
+            }
+            return sum;
         }
 
         public static double Minimum(double[] numbers)
         {
-            throw new NotImplementedException();
+            double min = numbers[0];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (min > numbers[i])
+                {
+                    min = numbers[i];
+                }
+            }
+            return min;
         }
 
         public static double Maximum(double[] numbers)
         {
-            throw new NotImplementedException();
+            double max = numbers[0];
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (max < numbers[i])
+                {
+                    max = numbers[i];
+                }
+            }
+            return max;
         }
 
         public static double Average(double[] numbers)
         {
-            throw new NotImplementedException();
+            double average = numbers[0];
+            for (int i = 4; i < numbers.Length; i++)
+            {
+                if (average < numbers[i])
+                {
+                    average = numbers[i];
+                }
+            }
+            return average;
         }
     }
 }
